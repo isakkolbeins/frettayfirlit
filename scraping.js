0e4e8ead936f467113075a7d3b46c9cf7808a536
sakkattack <32710497+sakkattack@users.noreply.github.com>
2017-12-26 23:30:19 +0000

    Add files via upload

--- scraping.js
+++ scraping.js
@@ -4,31 +4,30 @@
   var ruv = document.querySelector('.RUV');
 */
 
+var hedlines = [];
 
-
-// getMBL();
+getMBL();
 getVISIR();
+console.log(hedlines);
+//var f = getVISIR();
+//console.log(f);
 
 function getMBL() {
 
   var request = require('request');
   var cheerio = require('cheerio');
-  var hedlines= [];
+  //var hedlines= [];
   request('http://www.mbl.is/frettir/', function(err, resp, body){
     if(!err && resp.statusCode == 200){
-      //console.log('running');
+      console.log('running MBL');
       let $ = cheerio.load(body);
       $('a.fgc').each(function(i, val){
         var hl = $(this).attr('title');
+        var link = $(this).attr('href');
         if (typeof hl !== typeof undefined && hl !== false) {
-          //hedlines.push(this);
-          console.log(hl);
-          console.log($(this).attr('href'));
-          hedlines.push(hl);
-
+          window.hedlines.push(hl , 'http://www.mbl.is/frettir/' + link);
         }
       });
-      // console.log(hedlines);
       // console.log(hedlines.length);
       //addToHTML(hedlines, mbl);
     }
@@ -48,36 +47,26 @@ function addToHTML(hl, site){
 function getVISIR() {
   var request = require('request');
   var cheerio = require('cheerio');
-  var hedlines= [];
+  //var hedlines= [];
   request('http://www.visir.is/', function(err, resp, body){
     if(!err && resp.statusCode == 200){
-      //console.log('running');
+      console.log('running VISIR');
       let $ = cheerio.load(body);
-      $('.article-item__title').each(function(i, val){
-
-
+      $('.article-item__title a').each(function(i, val){
         var link = $(this).attr('href');
-        // console.log(link);
         var hl = $(this).text();
-        if (hl != '') {
 
-          //hedlines.push(this);
-          hedlines.push(hl.trim());
+        if (hl != ''
+            && link.startsWith('/g/')
+            && !window.hedlines.includes('http://www.visir.is' + link)) {
+
+          // Safnar - 0 fyrirsögn - 1 link osfv.
+          window.hedlines.push(hl.trim(),'http://www.visir.is' + link);
         }
 
       });
-      console.log(hedlines);
-      console.log(hedlines.length);
-      //addToHTML(hedlines, mbl);
     }
   });
-
-
-
-
-
-
-
 
 
 }
